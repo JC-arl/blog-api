b97279db57b4ecbc72fe31ac1ae40fad46bb3b30
JC-arl <hwang1535@naver.com>
2025-12-26 06:29:43 +0900

    testing categoryControllerTest TestSecurityConfig 추가

--- src/test/java/com/wsd/blogapi/category/CategoryControllerTest.java
+++ src/test/java/com/wsd/blogapi/category/CategoryControllerTest.java
@@ -3,12 +3,14 @@ package com.wsd.blogapi.category;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wsd.blogapi.category.dto.CreateCategoryRequest;
 import com.wsd.blogapi.security.JwtProvider;
+import com.wsd.blogapi.security.TestSecurityConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 @Transactional
 class CategoryControllerTest {
 
