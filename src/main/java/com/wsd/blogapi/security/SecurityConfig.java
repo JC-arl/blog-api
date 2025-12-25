12d1c15758d6f83fa076519497aaae8b9b314c2e
JC-arl <hwang1535@naver.com>
2025-12-26 06:31:40 +0900

    securityconfig @Profile("!test") 테스팅에서는 무시

--- src/main/java/com/wsd/blogapi/security/SecurityConfig.java
+++ src/main/java/com/wsd/blogapi/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @RequiredArgsConstructor
+@Profile("!test")
 public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
