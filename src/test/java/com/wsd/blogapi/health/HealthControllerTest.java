08cef27e45d4c26a025e7a84206eca3eae13c0ae
JC-arl <hwang1535@naver.com>
2025-12-26 06:30:45 +0900

    testing healthControllerTest TestSecurityConfig 어노테이션 추가

--- src/test/java/com/wsd/blogapi/health/HealthControllerTest.java
+++ src/test/java/com/wsd/blogapi/health/HealthControllerTest.java
@@ -1,10 +1,12 @@
 package com.wsd.blogapi.health;
 
+import com.wsd.blogapi.security.TestSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class HealthControllerTest {
 
     @Autowired
