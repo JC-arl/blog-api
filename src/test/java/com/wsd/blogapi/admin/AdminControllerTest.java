45223ae8dad26b370f139e76ad4370752c8113dd
JC-arl <hwang1535@naver.com>
2025-12-26 06:28:09 +0900

    testing AdminControllerTest 응답 수정

--- src/test/java/com/wsd/blogapi/admin/AdminControllerTest.java
+++ src/test/java/com/wsd/blogapi/admin/AdminControllerTest.java
@@ -1,12 +1,14 @@
 package com.wsd.blogapi.admin;
 
 import com.wsd.blogapi.security.JwtProvider;
+import com.wsd.blogapi.security.TestSecurityConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 @Transactional
 class AdminControllerTest {
 
