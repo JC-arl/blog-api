5ad2b60291b9e4661a056c024ac9c28cc61045a6
JC-arl <hwang1535@naver.com>
2025-12-26 06:33:02 +0900

    testing blogapplication  @Import(TestSecurityConfig.class) 어노테이션 추가

--- src/test/java/com/wsd/blogapi/BlogApiApplicationTests.java
+++ src/test/java/com/wsd/blogapi/BlogApiApplicationTests.java
@@ -1,11 +1,14 @@
 package com.wsd.blogapi;
 
+import com.wsd.blogapi.security.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class BlogApiApplicationTests {
 
     @Test
