81a5f3ca98d5a59862a6dd8869cedcc323ab80db
JC-arl <hwang1535@naver.com>
2025-12-26 03:01:46 +0900

    migration에 관리자 추가

--- src/main/resources/db/migration/V2__seed_users.sql
+++ src/main/resources/db/migration/V2__seed_users.sql
@@ -3,13 +3,4 @@
 -- =========================
 INSERT INTO users (email, password_hash, nickname, role, status, provider, provider_id)
 VALUES
-    ('admin1@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자1', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin1'),
-    ('admin2@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자2', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin2'),
-    ('admin3@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자3', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin3'),
-    ('admin4@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자4', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin4'),
-    ('admin5@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자5', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin5'),
-    ('admin6@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자6', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin6'),
-    ('admin7@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자7', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin7'),
-    ('admin8@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자8', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin8'),
-    ('admin9@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자9', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin9'),
-    ('admin10@club.com', '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자10','ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin10');
+    ('admin@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자1', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin1'),
