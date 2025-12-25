52048d6b2feb19b404e39b8bc555bec101976919
JC-arl <hwang1535@naver.com>
2025-12-26 02:59:04 +0900

    updated 댓글 request

---
+++ src/main/java/com/wsd/blogapi/comment/dto/UpdateCommentRequest.java
@@ -0,0 +1,11 @@
+package com.wsd.blogapi.comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateCommentRequest(
+        @NotBlank(message = "댓글 내용은 필수입니다")
+        @Size(min = 1, max = 1000, message = "댓글은 1~1000자 이내여야 합니다")
+        String content
+) {
+}
