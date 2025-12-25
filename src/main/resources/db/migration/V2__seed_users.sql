cd41c93d1cc4480438478702cca7abac2b140435
JC-arl <hwang1535@naver.com>
2025-12-26 04:58:28 +0900

    마이그레이션 user 이메일 수정

--- src/main/resources/db/migration/V2__seed_users.sql
+++ src/main/resources/db/migration/V2__seed_users.sql
@@ -1,6 +1,6 @@
 -- =========================
--- ADMIN USERS (10)
+-- ADMIN USERS (1)
 -- =========================
 INSERT INTO users (email, password_hash, nickname, role, status, provider, provider_id)
 VALUES
-    ('admin@club.com',  '$2a$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36Ew5z9z6pG9v7K4l1kK9iK', '관리자1', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin1'),
+    ('admin@blog.com',  '$2a$10$yFNjUlpLZ3d.IBGJoCtHqeGEDUBmE7tW4B0SGtClR8OD1RxNLUr6q', '관리자', 'ROLE_ADMIN', 'ACTIVE', 'LOCAL', 'admin');
