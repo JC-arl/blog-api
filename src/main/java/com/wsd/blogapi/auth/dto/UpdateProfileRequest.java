4db01856d5254a75327d3d3950e20996d47055c1
JC-arl <hwang1535@naver.com>
2025-12-26 19:50:18 +0900

    내 정보 조회 및 수정 swagger반영

--- src/main/java/com/wsd/blogapi/auth/dto/UpdateProfileRequest.java
+++ src/main/java/com/wsd/blogapi/auth/dto/UpdateProfileRequest.java
@@ -1,9 +1,12 @@
 package com.wsd.blogapi.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "프로필 수정 요청")
 public class UpdateProfileRequest {
+    @Schema(description = "닉네임", example = "새로운닉네임", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank(message = "닉네임은 필수입니다")
     @Size(min = 2, max = 50, message = "닉네임은 2~50자 사이여야 합니다")
     private String nickname;
